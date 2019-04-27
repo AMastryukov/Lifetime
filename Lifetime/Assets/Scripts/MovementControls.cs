269df47a24c7db2b21d97e5d6504d5006b974d31
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2019-04-27 11:00:40 -0400

    Add basic movement controls

---
+++ Lifetime/Assets/Scripts/MovementControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControls : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private Vector2 directionVector;
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + directionVector * moveSpeed * Time.deltaTime);
+    }
+
+    public void SetDirectionVector(Vector2 newDirectionVector)
+    {
+        directionVector = newDirectionVector;
+        directionVector.Normalize();
+    }
+}
---
+++ Lifetime/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private MovementControls movementControls;
+
+    private float horizontalInput, verticalInput;
+    
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        movementControls.SetDirectionVector(new Vector2(horizontalInput, verticalInput));
+    }
+}
