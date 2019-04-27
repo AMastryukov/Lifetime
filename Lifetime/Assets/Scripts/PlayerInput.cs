a200b5885fbecacbff1ef2f35ab402a1697ca67a
Zuhair <zuhair.ghias@hotmail.com>
2019-04-27 12:55:13 -0400

    Configured basic tile palette

---
+++ Lifetime/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    private PlayerMovement movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float vInput = Input.GetAxisRaw("Vertical");
+
+        movement.setMovement(new Vector2(hInput, vInput));
+
+
+    }
+}
---
+++ Lifetime/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 1;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed);
+    }
+
+    public void setMovement(Vector2 movement) {
+        this.movement = movement;
+        movement.Normalize();
+    }
+}
