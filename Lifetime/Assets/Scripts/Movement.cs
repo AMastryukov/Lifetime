939d93f6adc59598a0d4d779fb7a1e522dd3f432
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2019-04-27 14:18:09 -0400

    Add Player and Camera prefab - Fix bug where player spins after death

--- Lifetime/Assets/Scripts/Movement.cs
+++ Lifetime/Assets/Scripts/Movement.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rigidBody;
     [SerializeField] private float moveSpeed = 1f;
 
     private Vector2 directionVector;
-    private Rigidbody2D rigidBody;
-
-    private void Start()
-    {
-        rigidBody = GetComponent<Rigidbody2D>();
-    }
+    private Vector3 lookPosition = Vector3.zero;
 
     private void FixedUpdate()
     {
         rigidBody.MovePosition(rigidBody.position + directionVector * moveSpeed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - lookPosition, Vector3.forward);
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
     }
 
     public void SetDirectionVector(Vector2 newDirectionVector)
@@ -27,7 +29,6 @@ public class Movement : MonoBehaviour
 
     public void SetLookPosition(Vector3 newLookPosition)
     {
-        transform.rotation = Quaternion.LookRotation(transform.position - newLookPosition, Vector3.forward);
-        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        lookPosition = newLookPosition;
     }
 }
