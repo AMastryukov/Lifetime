32c94385d553547469e564d4cf1e5c0283ceb751
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2019-04-27 12:51:54 -0400

    Implement IWeapon interface in SpecialWeapon

--- Lifetime/Assets/Scripts/Weapons/SpecialWeapon.cs
+++ Lifetime/Assets/Scripts/Weapons/SpecialWeapon.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpecialWeapon : MonoBehaviour
+public class SpecialWeapon : MonoBehaviour, IWeapon
 {
     public float damage;
 
