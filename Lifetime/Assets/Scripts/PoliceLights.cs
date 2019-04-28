a04ffb1c7115b09f72cc49ca2b0b999b671d2721
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2019-04-28 14:00:05 -0400

    Add new level - Create new level (Market) - Add destructible crates - Add police vehicles (props) - Adjust some textures to be darker - Rework knockback force behavior

---
+++ Lifetime/Assets/Scripts/PoliceLights.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceLights : MonoBehaviour
+{
+    [SerializeField] private Light blue, red;
+    [SerializeField] private float flashInterval = 0.5f;
+
+    private void Start()
+    {
+        blue.enabled = true;
+        red.enabled = false;
+
+        StartCoroutine(FlashLights());
+    }
+
+    private IEnumerator FlashLights()
+    {
+        while(true)
+        {
+            blue.enabled = !blue.enabled;
+            red.enabled = !red.enabled;
+
+            yield return new WaitForSeconds(flashInterval);
+        }
+    }
+}
